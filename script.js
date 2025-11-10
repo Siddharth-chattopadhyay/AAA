8a5d8ddd8f3d5aad9ea0254ea9f4129978336f1f
Siddharth Chattopadhyay <siddh@Nabanitas-MacBook-Air.local>
2025-11-10 10:02:06 +0530

    Done

--- script.js
+++ script.js
@@ -12,20 +12,54 @@ const images = [
         "caption": "Japan Temple"
     }
 ];
+const imageObj = document.querySelector("#carousel > img");
+const imageCaption = document.querySelector("#textfield > #caption");
+const next = document.getElementById("next");
+const prev = document.getElementById("prev");
+const playstatebtn = document.getElementById("playstate");
+const errorField = document.getElementById("error");
 
-function setImage(img, caption) {
+const newPath = document.getElementById("path");
+const newCap = document.getElementById("uploadcaption");
+const finalUploadBtn = document.getElementById("upload");
+
+const uploadBox = document.getElementById("newimageform");
+
+const showUpload = document.getElementById("sup");
+const hideUpload = document.getElementById("uploadClose");
+
+let isPaused = true;
+let intervalHandle = undefined;
+let index = 0;
+
+showUpload.addEventListener("click", () => {
+    uploadBox.style.display = "flex";
+});
+
+finalUploadBtn.addEventListener("click", () => {
+    images.push({"img": newPath.value, "caption": newCap.value});
+    newPath.value = newCap.value = "";
+    uploadBox.style.display = "none";
+    setImageIndex(index);
+});
+
+
+hideUpload.addEventListener("click", () => {
+    uploadBox.style.display = "none";
+});
+
+
+
+
+function showSlide(img, caption) {
     imageObj.setAttribute("src", img);
     imageObj.setAttribute("alt", caption);
     imageCaption.innerHTML = caption;
 }
 
-const imageObj = document.querySelector("#carousel > img");
-const imageCaption = document.querySelector("#carousel > #caption");
-
-let isPaused = false;
-
-const playstatebtn = document.getElementById("playstate");
 playstatebtn.addEventListener("click", function(){
+    if (intervalHandle === undefined)
+        intervalHandle = setInterval(interval, 1000 * 3);
     isPaused = !isPaused;
     if (isPaused)
         playstatebtn.innerHTML = "Play";
@@ -33,10 +67,52 @@ playstatebtn.addEventListener("click", function(){
         playstatebtn.innerHTML = "Pause"; 
 });
 
-let index = 0;
-setImage(images[index].img, images[index++].caption);
-setInterval(function () {
+function setImageIndex(index){
+    if (index < 0) {
+        errorField.innerHTML = "This is the first slide";
+        return 0;
+    }
+    if (index === images.length) {
+        errorField.innerHTML = "This is the last slide";
+        return images.length - 1;
+    }
+    if (index > images.length) {
+        errorField.innerHTML = "Invalid slide index";
+        return images.length - 1;
+    }
+    else {
+        showSlide(images[index].img, `${images[index].caption} [${index + 1}/${images.length}]`);
+        errorField.innerHTML = "";
+        return index;
+    }
+}
+
+function interval() {
+    if (index >= images.length - 1) {
+        clearInterval(intervalHandle);
+        playstatebtn.innerHTML = "Auto Play";
+        intervalHandle = undefined;
+        isPaused = !isPaused;
+        return;
+    };
     if (isPaused) return;
-    const select = images[index++ % images.length];
-    setImage(select.img, select.caption);
-}, 1000 * 3);
+    index = setImageIndex(++index);
+}
+
+index = setImageIndex(index);
+
+next.addEventListener("click", function(){
+    if (intervalHandle !== undefined){
+        clearInterval(intervalHandle);
+        intervalHandle = setInterval(interval, 1000 * 3);
+    }
+    index = setImageIndex(++index);
+});
+
+prev.addEventListener("click", function(){
+    if (intervalHandle !== undefined){
+        clearInterval(intervalHandle);
+        intervalHandle = setInterval(interval, 1000 * 3);
+    }
+    index = setImageIndex(--index);
+});
